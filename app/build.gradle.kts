76eae57c611bd1b092cf8f39d823be0345934ad2
Rodrigo Costa <rodrigocosta.rsc2878@gmail.com>
2026-09-13 13:23:04 +0100

    dependencies for room caching

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -51,4 +52,7 @@ dependencies {
     implementation(libs.retrofit.core)
     implementation(libs.okhttp.core)
     testImplementation(libs.kxml2)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 }
